4b00d8a74802230a32ca92059c30b513dffcc4df
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-12 20:08:29 -0400

    Homepage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
+import HomePage from './homepage.component';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <HomePage/>
   );
 }
 
