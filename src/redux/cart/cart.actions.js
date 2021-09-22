404f4c484a22163aa8518b373c68d1a30977c23c
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-22 12:55:31 -0400

    implemented remove item from checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = item => ({
   type: CartActionsType.ADD_ITEM,
   payload: item
 })
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionsType.CLEAR_ITEM_FROM_CART,
+  payload: item
+})
