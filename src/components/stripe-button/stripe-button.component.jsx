fc19f9a54b5e8381edb62b584d638f551212fe51
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-27 16:36:51 -0400

    Updating strip integration and update favicon.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100
+  const publishableKey = 'pk_test_51HKDkpCU5Zb8g0e6WPfe7zkzqEr6YfOcFT5LhOP8oe7dmTsPiBwPBCsCv1oIToDfecGkfDk3A57fJnUFyf3lLsUN00Lu6noUWC'
+
+  const onToken = token => {
+    console.log(token)
+    alert('Payment Successful')
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is: $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton
