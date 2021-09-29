e101cb79f8dc37463f920761764135526e036b2b
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-29 16:34:17 -0400

    add redux thunk to the app to handle async code when fetching data from database

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,30 @@
 import ShopActionsType from "./shop.types";
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionsType.UPDATE_COLLECTIONS,
+import { collection, getDocs } from "@firebase/firestore";
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionsType.FETCH_COLLECTIONS_START
+})
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionsType.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 })
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionsType.FETCH_COLLECTIONS_FEILURE,
+  payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = collection(firestore, 'collections')
+    dispatch(fetchCollectionsStart())
+
+    getDocs(collectionRef).then( snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      dispatch(fetchCollectionsSuccess(collectionsMap))
+    }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+  }
+}
